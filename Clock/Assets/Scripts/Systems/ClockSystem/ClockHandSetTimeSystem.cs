58bee30b3dd2845d9c23888c92fd0135076ca2c4
Maksim S <maxsic1985@gmail.com>
2024-01-16 20:24:16 +0300

    add set  minute

--- Clock/Assets/Scripts/Systems/ClockSystem/ClockHandSetTimeSystem.cs
+++ Clock/Assets/Scripts/Systems/ClockSystem/ClockHandSetTimeSystem.cs
@@ -14,8 +14,10 @@ namespace MSuhinin.Clock
         private EcsPool<IsHandSetTimeComponent> _isHandSetTimeComponent;
         private EcsPool<ClockViewComponent> _clockViewComponentPool;
         private EcsPool<TimeComponent> _timeComponentPool;
-        private int curAngle;
-        private int angle;
+        private int _curHourAngle;
+        private int _hourAngle;  
+        private int _curMinAngle;
+        private int _minAngle;
         private Vector3 cur;
         private GameSharedData _sharedData;
 
@@ -54,13 +56,12 @@ namespace MSuhinin.Clock
                 {
                     ref var _setTimeComponent = ref _isHandSetTimeComponent.Add(entity);
 
-                    curAngle = Mathf.RoundToInt(clockViewComponentPool
-                        .HoursEuler.transform.rotation.eulerAngles.z);
-                    angle = curAngle;
-                    Debug.Log("Curangle" + curAngle);
-                    Debug.Log("angle" + angle);
-                    cur = clockViewComponentPool
-                        .HoursEuler.transform.rotation.eulerAngles;
+                    _curHourAngle = Mathf.RoundToInt(clockViewComponentPool.HoursEuler.transform.rotation.eulerAngles.z);
+                    _hourAngle = _curHourAngle;
+                    
+                    _curMinAngle = Mathf.RoundToInt(clockViewComponentPool.MinutesEuler.transform.rotation.eulerAngles.z);
+                    _minAngle = _curMinAngle;
+  
                 }
             }
 
@@ -73,99 +74,118 @@ namespace MSuhinin.Clock
                     foreach (var timeEntity in _timeFilter)
                     {
                         ref var time = ref _timeComponentPool.Get(timeEntity);
-                        //  Debug.Log(clockViewComponentPool.HoursEuler.transform.rotation.eulerAngles.z);
 
-
-                        angle = Mathf.RoundToInt(clockViewComponentPool.HoursEuler.localRotation.eulerAngles.z);
-                        // var angle = Mathf.RoundToInt(clockViewComponentPool.HoursEuler.rotation.eulerAngles.z);
-
-                        if (angle == curAngle)
-                        {
-                            return;
-                        }
-
-                        Debug.Log("Direction" + _sharedData.GetMouseDirection);
-                        var summ = curAngle + angle;
-
-
-                        if (_sharedData.GetMouseDirection)
-                        {
-                            if (curAngle < 180 && angle < 180)
-                            {
-                                var addh = (curAngle - angle) / 30;
-                                time.HOUR = time.HOUR + addh;
-                             
-                            }
-                            else if (curAngle < 180 && angle > 180)
-                            {
-                                var addh = ((360 - angle) + curAngle) / 30;
-                                time.HOUR = time.HOUR + addh;
-                          
-                            }
-                            else if (curAngle > 180 && angle > 180)
-                            {
-                                if (curAngle > angle)
-                                {
-                                    var addh = (curAngle - angle) / 30;
-                                    time.HOUR = time.HOUR + addh;
-                                
-                                }
-                            }
-                            else if (curAngle > 180 && angle < 180)
-                            {
-                                var addh = (curAngle - angle) / 30;
-                                time.HOUR = time.HOUR + addh;
-                            
-                            }
-                        }
-                        else
-                        {
-                            if (curAngle < 180 && angle < 180)
-                            {
-                                var addh = (angle - curAngle) / 30;
-                                time.HOUR = time.HOUR - addh;
-                              
-                            }
-                            else if (curAngle < 180 && angle > 180)
-                            {
-                                if (curAngle < angle)
-                                {
-                                    var addh = (angle - curAngle) / 30;
-                                    time.HOUR = time.HOUR - addh;
-                              
-                                }
-                            }
-                            else if (curAngle > 180 && angle > 180)
-                            {
-                                var addh = (angle - curAngle) / 30;
-                                time.HOUR = time.HOUR - addh;
-                            }
-                            else if (curAngle > 180 && angle < 180)
-                            {
-                                var addh = ((360 - curAngle) + angle) / 30;
-                                time.HOUR = time.HOUR - addh;
-                            }
-                        }
-
-               
-                        if (time.HOUR > 23)
-                        {
-                            time.HOUR = time.HOUR > 23 ? time.HOUR - 23 : time.HOUR;
-                        }
-                        
-                        if (time.HOUR < 0)
-                        {
-                            time.HOUR = time.HOUR < 0 ? time.HOUR + 23 : time.HOUR;
-                        }
-
-                        //1 берем угол стрелки
-                        //2 прибавляем или убавляем если угол уменьшается то прибавляем
-//тек время - или + угол/30 
-//тек угол -новый угол = 30= 1 час 
+                        SetHoursValue(clockViewComponentPool, ref time);
+                        SetMinutesValue(clockViewComponentPool, ref time);
                     }
 
                     _isHandSetTimeComponent.Del(entity);
                 }
+            }
+        }
+
+        private void SetHoursValue(ClockViewComponent clockViewComponentPool, ref TimeComponent time)
+        {
+            _hourAngle = Mathf.RoundToInt(clockViewComponentPool.HoursEuler.localRotation.eulerAngles.z);
+
+            if (_hourAngle == _curHourAngle)
+            {
+                return;
+            }
+
+
+            if (_sharedData.GetMouseDirection)
+            {
+                if (_curHourAngle < 180 && _hourAngle < 180)
+                {
+                    var addh = (_curHourAngle - _hourAngle) / 30;
+                    time.HOUR = time.HOUR + addh;
+                }
+                else if (_curHourAngle < 180 && _hourAngle > 180)
+                {
+                    var addh = ((360 - _hourAngle) + _curHourAngle) / 30;
+                    time.HOUR = time.HOUR + addh;
+                }
+                else if (_curHourAngle > 180 && _hourAngle > 180)
+                {
+                    if (_curHourAngle > _hourAngle)
+                    {
+                        var addh = (_curHourAngle - _hourAngle) / 30;
+                        time.HOUR = time.HOUR + addh;
+                    }
+                }
+                else if (_curHourAngle > 180 && _hourAngle < 180)
+                {
+                    var addh = (_curHourAngle - _hourAngle) / 30;
+                    time.HOUR = time.HOUR + addh;
+                }
+            }
+            else
+            {
+                if (_curHourAngle < 180 && _hourAngle < 180)
+                {
+                    var addh = (_hourAngle - _curHourAngle) / 30;
+                    time.HOUR = time.HOUR - addh;
+                }
+                else if (_curHourAngle < 180 && _hourAngle > 180)
+                {
+                    if (_curHourAngle < _hourAngle)
+                    {
+                        var addh = (_hourAngle - _curHourAngle) / 30;
+                        time.HOUR = time.HOUR - addh;
+                    }
+                }
+                else if (_curHourAngle > 180 && _hourAngle > 180)
+                {
+                    var addh = (_hourAngle - _curHourAngle) / 30;
+                    time.HOUR = time.HOUR - addh;
+                }
+                else if (_curHourAngle > 180 && _hourAngle < 180)
+                {
+                    var addh = ((360 - _curHourAngle) + _hourAngle) / 30;
+                    time.HOUR = time.HOUR - addh;
+                }
+            }
+
+            if (time.HOUR > 23)
+            {
+                time.HOUR = time.HOUR > 23 ? time.HOUR - 23 : time.HOUR;
+            }
+
+            if (time.HOUR < 0)
+            {
+                time.HOUR = time.HOUR < 0 ? time.HOUR + 23 : time.HOUR;
+            }
+        }
+
+        private void SetMinutesValue(ClockViewComponent clockViewComponentPool, ref TimeComponent time)
+        {
+            _minAngle = Mathf.RoundToInt(clockViewComponentPool.MinutesEuler.localRotation.eulerAngles.z);
+
+            if (_minAngle == _curMinAngle)
+            {
+             //   return;
+            }
+            
+            if (_sharedData.GetMouseDirection)
+            {
+                var addmin = (_curMinAngle - _minAngle) / 6;
+                time.MIN = time.MIN + addmin;
+            }
+            else
+            {
+                var addmin = (_minAngle-_curMinAngle) / 6;
+                time.MIN = time.MIN - addmin;
+            }
+
+            if (time.MIN > 59)
+            {
+                time.MIN = time.MIN > 59 ? time.MIN - 59 : time.MIN;
+            }
+            
+            if (time.MIN < 1)
+            {
+                time.MIN = time.MIN < 1 ? time.MIN + 59 : time.MIN;
             }
         }
     }
